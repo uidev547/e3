926094b94e807262bd3a3f20f27daac9d3f9a8bd
Kiran <akiran547@gmail.com>
2017-10-27 13:25:44 +0530

    m

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var index = require('./routes/index');
 var users = require('./routes/users');
+var path = require('path');
 module.exports = {
 	init: function function_name(app) {
 		app.use('/e3', index);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'E3 Express' });
+  res.render(path.join(__dirname, '../views/index'), { title: 'E3 Express' });
 });
 
 module.exports = router;
